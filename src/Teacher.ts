390ca132818b73a1e8fb05e106091e6d56b34a00
Diego Molinari <63561210+diegotjmolinari@users.noreply.github.com>
2020-09-04 11:05:05 -0300

    data nacimento professor

--- src/Teacher.ts
+++ src/Teacher.ts
@@ -6,6 +6,7 @@ export class Teacher implements User {
     public id: string,
     public name: string,
     public email: string,
+    public birthDate: moment.Moment,
     public specialties: TEACHER_SPECIALTY[],
   ) {}
 }
@@ -21,17 +22,22 @@ export enum TEACHER_SPECIALTY {
   BACKEND = 'BACKEND',
 }
 
-export const teacher1 = new Teacher('1', 'Florinda', 'florinda@gmail.com', [
+export const teacher1 = new Teacher('1', 'Florinda', 'florinda@gmail.com',  moment('03/08/1981', 'DD/MM/YYYY'), [
   TEACHER_SPECIALTY.TESTES,
   TEACHER_SPECIALTY.CSS,
   TEACHER_SPECIALTY.REDUX,
 ]);
-export const teacher2 = new Teacher('2', 'Madruga', 'madruga@gmail.com', [
+export const teacher2 = new Teacher('2', 'Madruga', 'madruga@gmail.com', moment('03/08/1980', 'DD/MM/YYYY'), [
   TEACHER_SPECIALTY.REACT,
   TEACHER_SPECIALTY.BACKEND,
   TEACHER_SPECIALTY.REDUX,
 ]);
-export const teacher3 = new Teacher('3', 'Girafales', 'girafales@gmail.com', [
+export const teacher3 = new Teacher('3', 'Girafales', 'girafales@gmail.com', moment('03/08/1989', 'DD/MM/YYYY'), [
+  TEACHER_SPECIALTY.TESTES,
+  TEACHER_SPECIALTY.OOP,
+]);
+
+export const teacher4 = new Teacher('4', 'Clotilde', 'clotilde@gmail.com', moment('03/08/1985', 'DD/MM/YYYY'), [
   TEACHER_SPECIALTY.TESTES,
   TEACHER_SPECIALTY.OOP,
 ]);
